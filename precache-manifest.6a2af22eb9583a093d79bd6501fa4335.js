ac854696b954c5ec9e79073d1bcb5f317f9b4770
uzardo <sebastian_uz@yahoo.pl>
2019-11-05 16:41:33 +0100

    Updates

---
+++ precache-manifest.6a2af22eb9583a093d79bd6501fa4335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5aaa4b3493a48097fcde0b6272ec9f",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "1f0d57cf3eaf98278178",
+    "url": "/users/static/css/main.bdbd903b.chunk.css"
+  },
+  {
+    "revision": "ebff82f64690a9867108",
+    "url": "/users/static/js/2.fd448b80.chunk.js"
+  },
+  {
+    "revision": "1f0d57cf3eaf98278178",
+    "url": "/users/static/js/main.79e32bd9.chunk.js"
+  },
+  {
+    "revision": "a909391fbaa1ec184ed0",
+    "url": "/users/static/js/runtime-main.634616e6.js"
+  }
+]);
